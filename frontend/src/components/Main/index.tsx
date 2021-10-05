83910b826a6749f3ad647e6fe59d96d90accfe5b
Marcos Vinicius <marcos_muppet@outlook.com>
2021-10-05 19:48:26 -0300

    Links desabilitados

--- frontend/src/components/Main/index.tsx
+++ frontend/src/components/Main/index.tsx
@@ -6,7 +6,7 @@ function Main() {
                     <h1>Cover your page.</h1>
                     <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
                     <p className="lead">
-                        <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+                        <a href="/" className="isDisabled btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
                     </p>
                 </main>
             </div>
--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -4,15 +4,15 @@ function NavBar() {
             <div className="container">
                 <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                        <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlinkHref="#bootstrap" /></svg>
+                        <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlinkHref="/bootstrap" /></svg>
                     </a>
 
                     <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
-                        <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
-                        <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
-                        <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
-                        <li><a href="#" className="nav-link px-2 text-white">About</a></li>
+                        <li><a href="/" className="isDisabled nav-link px-2 text-secondary">Home</a></li>
+                        <li><a href="/" className="isDisabled nav-link px-2 text-white">Features</a></li>
+                        <li><a href="/" className="isDisabled nav-link px-2 text-white">Pricing</a></li>
+                        <li><a href="/" className="isDisabled nav-link px-2 text-white">FAQs</a></li>
+                        <li><a href="/" className="isDisabled nav-link px-2 text-white">About</a></li>
                     </ul>
                     
                     <div className="text-end">
