e7e9009c1f5dd362349ccbf5c2e2c4fe7fa95e52
Sadraque Bezerra <sadraque.bezerra23@gmail.com>
2021-10-21 14:32:57 -0300

    project create

---
+++ backend/src/main/java/com/labes/monitoramentopet/MonitoramentoPetApplication.java
@@ -0,0 +1,13 @@
+package com.labes.monitoramentopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoPetApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/labes/monitoramentopet/MonitoramentoPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labes.monitoramentopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
