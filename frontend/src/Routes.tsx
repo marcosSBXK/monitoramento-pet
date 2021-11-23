cab3977fa054cdaccbf989a99c18a81f7e61511e
Sadraque Bezerra <sadraque.bezerra23@gmail.com>
2021-11-23 10:16:15 -0300

    login

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,23 +1,30 @@
 import Main from "components/Main";
-import Home from "components/User/Login/login";
+//import Home from "components/User/Login/login";
 import 'assets/css/styles.css';
 
 
 import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { Login } from "components/User/Login/login";
+import { AuthProvider } from "context/AuthProvider";
+import { ProtectedLayout } from "components/ProtectedLayout";
 
 const Routes = () => {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/Main" >
-                    <Main />
-                </Route>
-                
-            </Switch>        
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/' exact>
+                        <Login />
+                    </Route>
+                    <Route path='/main' >
+                        <ProtectedLayout>                  
+                                <Main />  
+                        </ProtectedLayout>
+                    </Route>
+                </Switch>
+
+            </BrowserRouter>
+        </AuthProvider>
     );
 }
 
---
+++ frontend/src/service/api.ts
@@ -0,0 +1,27 @@
+
+import axios from 'axios';
+import { getUserLocalStorage } from 'context/AuthProvider/util';
+
+
+
+export const Api = axios.create({
+    baseURL: "https://reqres.in/api/",
+});
+
+
+
+Api.interceptors.request.use(
+    (config) => {
+        const user = getUserLocalStorage()
+
+        config.headers = {
+            Authorization: user?.token,
+        };
+        
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
